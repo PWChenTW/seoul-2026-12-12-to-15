babd6c0c2163db735b25a4f58848d1f7df01d0f1
PWChenTW <81994394+PWChenTW@users.noreply.github.com>
2026-08-29 21:23:11 +0800

    Add Google Maps routes and food photo galleries

---
+++ food-photos.js
@@ -0,0 +1,393 @@
+window.FOOD_PHOTOS = {
+  "1": [
+    {
+      "src": "images/food/r01-1.jpg",
+      "title": "Soupcurry-hareruya",
+      "credit": "No machine-readable author provided. Oga~commonswiki assumed (based on copyright claims).",
+      "source": "https://commons.wikimedia.org/wiki/File:Soupcurry-hareruya.jpg",
+      "license": "CC BY-SA 3.0",
+      "licenseUrl": "http://creativecommons.org/licenses/by-sa/3.0/"
+    },
+    {
+      "src": "images/food/r01-2.jpg",
+      "title": "Rice and Soup curry with natto and okra by klm",
+      "credit": "klm*",
+      "source": "https://commons.wikimedia.org/wiki/File:Rice_and_Soup_curry_with_natto_and_okra_by_klm.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r01-3.jpg",
+      "title": "Soup curry (2604974894)",
+      "credit": "Yamaguchi Yoshiaki from Japan",
+      "source": "https://commons.wikimedia.org/wiki/File:Soup_curry_(2604974894).jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    }
+  ],
+  "2": [
+    {
+      "src": "images/food/r02-1.jpg",
+      "title": "Korean.dessert-Hoddeok-02",
+      "credit": "by Sung Sook",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean.dessert-Hoddeok-02.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r02-2.jpg",
+      "title": "Korean snack-Hotteok-01",
+      "credit": "by yearofeats",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_snack-Hotteok-01.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r02-3.jpg",
+      "title": "Hotteokfilling",
+      "credit": "keizie  / 정희 강 from Seoul, Korea",
+      "source": "https://commons.wikimedia.org/wiki/File:Hotteokfilling.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    }
+  ],
+  "3": [
+    {
+      "src": "images/food/r03-1.jpg",
+      "title": "Korean.food-Dakbokemtang-01",
+      "credit": "stuart_spivack (a flickr user)",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean.food-Dakbokemtang-01.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r03-2.jpg",
+      "title": "13-hadirector1 hoya00571 hoya0057",
+      "credit": "Derek Woodward",
+      "source": "https://commons.wikimedia.org/wiki/File:13-hadirector1_hoya00571_hoya0057.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    },
+    {
+      "src": "images/food/r03-3.jpg",
+      "title": "Daktoritang",
+      "credit": "Brücke-Osteuropa",
+      "source": "https://commons.wikimedia.org/wiki/File:Daktoritang.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    }
+  ],
+  "4": [
+    {
+      "src": "images/food/r04-1.jpg",
+      "title": "The Spicy One - Brand Sixteen 2024-03-08",
+      "credit": "Andy Li",
+      "source": "https://commons.wikimedia.org/wiki/File:The_Spicy_One_-_Brand_Sixteen_2024-03-08.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    },
+    {
+      "src": "images/food/r04-2.jpg",
+      "title": "Egg Sandwich 001",
+      "credit": "Ocdp",
+      "source": "https://commons.wikimedia.org/wiki/File:Egg_Sandwich_001.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    },
+    {
+      "src": "images/food/r04-3.jpg",
+      "title": "Bacon & egg sandwich - Monty's 2026-02-19",
+      "credit": "Andy Li",
+      "source": "https://commons.wikimedia.org/wiki/File:Bacon_%26_egg_sandwich_-_Monty%27s_2026-02-19.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    }
+  ],
+  "5": [
+    {
+      "src": "images/food/r05-1.jpg",
+      "title": "HK food sandwich egg tomato February 2022 Px3 05",
+      "credit": "PX3CO TURBO252400",
+      "source": "https://commons.wikimedia.org/wiki/File:HK_food_sandwich_egg_tomato_February_2022_Px3_05.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r05-2.jpg",
+      "title": "BEC Sandwich on Everything Bagel",
+      "credit": "Ilikefood",
+      "source": "https://commons.wikimedia.org/wiki/File:BEC_Sandwich_on_Everything_Bagel.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r05-3.jpg",
+      "title": "Egg Drop Sandwich and Fries",
+      "credit": "Marife.altabano",
+      "source": "https://commons.wikimedia.org/wiki/File:Egg_Drop_Sandwich_and_Fries.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    }
+  ],
+  "6": [
+    {
+      "src": "images/food/r06-1.jpg",
+      "title": "Grilled eel",
+      "credit": "Alpha from Melbourne, Australia",
+      "source": "https://commons.wikimedia.org/wiki/File:Grilled_eel.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r06-2.jpg",
+      "title": "Grilled eel at Tsukiji fish market in Tokyo, Japan in July 2026",
+      "credit": "Gatorfan252525",
+      "source": "https://commons.wikimedia.org/wiki/File:Grilled_eel_at_Tsukiji_fish_market_in_Tokyo,_Japan_in_July_2026.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r06-3.jpg",
+      "title": "Unagi Don - J-Pub Shogun AUD13 (3427209239)",
+      "credit": "Alpha from Melbourne, Australia",
+      "source": "https://commons.wikimedia.org/wiki/File:Unagi_Don_-_J-Pub_Shogun_AUD13_(3427209239).jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    }
+  ],
+  "7": [
+    {
+      "src": "images/food/r07-1.jpg",
+      "title": "Korean.cuisine-Anju-nogari.and.nuts",
+      "credit": "by ayustety",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean.cuisine-Anju-nogari.and.nuts.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r07-2.jpg",
+      "title": "Korean cuisine-Bulgogi-Nakji bokkeum",
+      "credit": "L. W. Yang",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_cuisine-Bulgogi-Nakji_bokkeum.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r07-3.jpg",
+      "title": "Korean cuisine-Anju-Golbaengi muchim-01",
+      "credit": "Bernat Agullo",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_cuisine-Anju-Golbaengi_muchim-01.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    }
+  ],
+  "8": [
+    {
+      "src": "images/food/r08-1.jpg",
+      "title": "Pizza food",
+      "credit": "Arungupta408",
+      "source": "https://commons.wikimedia.org/wiki/File:Pizza_food.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r08-2.jpg",
+      "title": "Super Supreme Pizza, Pizza Hut Perth, 2026 (01)",
+      "credit": "Bahnfrend",
+      "source": "https://commons.wikimedia.org/wiki/File:Super_Supreme_Pizza,_Pizza_Hut_Perth,_2026_(01).jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r08-3.jpg",
+      "title": "Pizza ( Italian Food)",
+      "credit": "SristiAwasthi21",
+      "source": "https://commons.wikimedia.org/wiki/File:Pizza_(_Italian_Food).jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    }
+  ],
+  "9": [
+    {
+      "src": "images/food/r09-1.jpg",
+      "title": "Korean fried chicken with honey mustard sauce",
+      "credit": "Fumikas Sagisavas",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_fried_chicken_with_honey_mustard_sauce.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    },
+    {
+      "src": "images/food/r09-2.jpg",
+      "title": "Honey Butter chicken from Korean Fried Chicken (2026-04-25)",
+      "credit": "Wide Awake!",
+      "source": "https://commons.wikimedia.org/wiki/File:Honey_Butter_chicken_from_Korean_Fried_Chicken_(2026-04-25).jpg",
+      "license": "CC BY 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/4.0"
+    },
+    {
+      "src": "images/food/r09-3.jpg",
+      "title": "Korean fried chicken with cheese flavor (Chikin)",
+      "credit": "Fumikas Sagisavas",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_fried_chicken_with_cheese_flavor_(Chikin).jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    }
+  ],
+  "10": [
+    {
+      "src": "images/food/r10-1.jpg",
+      "title": "Korean.food-Jokbal-01",
+      "credit": "by ayustety",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean.food-Jokbal-01.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r10-2.jpg",
+      "title": "Korean cuisine-Jokbal and saujeot",
+      "credit": "stu_spivack",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_cuisine-Jokbal_and_saujeot.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r10-3.jpg",
+      "title": "Korean cuisine-Jokbal-01",
+      "credit": "by aka_maya at Flickr",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_cuisine-Jokbal-01.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    }
+  ],
+  "13": [
+    {
+      "src": "images/food/r13-1.jpg",
+      "title": "Bagel sandwich meal prep",
+      "credit": "Revital Salomon",
+      "source": "https://commons.wikimedia.org/wiki/File:Bagel_sandwich_meal_prep.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r13-2.jpg",
+      "title": "Homemade salmon bagel sandwich 3",
+      "credit": "에그란",
+      "source": "https://commons.wikimedia.org/wiki/File:Homemade_salmon_bagel_sandwich_3.jpg",
+      "license": "CC BY 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/4.0"
+    },
+    {
+      "src": "images/food/r13-3.jpg",
+      "title": "Liat Portal for Foodie Disorder - Breakfast bagel sandwich in Tel Aviv",
+      "credit": "HaJunkiyada",
+      "source": "https://commons.wikimedia.org/wiki/File:Liat_Portal_for_Foodie_Disorder_-_Breakfast_bagel_sandwich_in_Tel_Aviv.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    }
+  ],
+  "14": [
+    {
+      "src": "images/food/r14-1.jpg",
+      "title": "00 Croissant. Yum",
+      "credit": "Mark Mitchell",
+      "source": "https://commons.wikimedia.org/wiki/File:00_Croissant._Yum.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r14-2.jpg",
+      "title": "APileOfCroissants",
+      "credit": "Raysonho @ Open Grid Scheduler / Grid Engine",
+      "source": "https://commons.wikimedia.org/wiki/File:APileOfCroissants.jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    },
+    {
+      "src": "images/food/r14-3.jpg",
+      "title": "Alisa Anton 2016-03-12 (Unsplash)",
+      "credit": "Alisa Anton alisaanton",
+      "source": "https://commons.wikimedia.org/wiki/File:Alisa_Anton_2016-03-12_(Unsplash).jpg",
+      "license": "CC0",
+      "licenseUrl": "http://creativecommons.org/publicdomain/zero/1.0/deed.en"
+    }
+  ],
+  "15": [
+    {
+      "src": "images/food/r15-1.jpg",
+      "title": "Apple Struddle with pistachio Gelato",
+      "credit": "Tammy Green from Chicago, USA",
+      "source": "https://commons.wikimedia.org/wiki/File:Apple_Struddle_with_pistachio_Gelato.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r15-2.jpg",
+      "title": "BrocatoGelato27Dec07",
+      "credit": "Infrogmation of New Orleans",
+      "source": "https://commons.wikimedia.org/wiki/File:BrocatoGelato27Dec07.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r15-3.jpg",
+      "title": "Caffe con gelato",
+      "credit": "Flickr: shiraga",
+      "source": "https://commons.wikimedia.org/wiki/File:Caffe_con_gelato.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    }
+  ],
+  "16": [
+    {
+      "src": "images/food/r16-1.jpg",
+      "title": "Korean.food-Seolleongtang-01",
+      "credit": "by abex (a flickr user)",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean.food-Seolleongtang-01.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    },
+    {
+      "src": "images/food/r16-2.jpg",
+      "title": "Korean soup-Seolleongtang-01",
+      "credit": "Mike Chung",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_soup-Seolleongtang-01.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r16-3.jpg",
+      "title": "Korean soup-Seolleongtang-02",
+      "credit": "Satomi Abe",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_soup-Seolleongtang-02.jpg",
+      "license": "CC BY-SA 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/2.0"
+    }
+  ],
+  "17": [
+    {
+      "src": "images/food/r17-1.jpg",
+      "title": "Korean barbecue - moksal, samgyeopsal, beoseot, and meljeot on a gridiron",
+      "credit": "대경라이프",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_barbecue_-_moksal,_samgyeopsal,_beoseot,_and_meljeot_on_a_gridiron.jpg",
+      "license": "CC BY-SA 4.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by-sa/4.0"
+    },
+    {
+      "src": "images/food/r17-2.jpg",
+      "title": "Korean barbecue-Samgyeopsal-02",
+      "credit": "정희 강",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_barbecue-Samgyeopsal-02.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    },
+    {
+      "src": "images/food/r17-3.jpg",
+      "title": "Korean Barbecue-Samgyeopsal-01",
+      "credit": "moso",
+      "source": "https://commons.wikimedia.org/wiki/File:Korean_Barbecue-Samgyeopsal-01.jpg",
+      "license": "CC BY 2.0",
+      "licenseUrl": "https://creativecommons.org/licenses/by/2.0"
+    }
+  ]
+};
+
